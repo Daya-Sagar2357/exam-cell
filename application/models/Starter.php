14398f1494179fdf3e5992428b4999c7e4a63da7
Daya-Sagar2357 <dayasagar2357@gmail.com>
2021-06-01 21:17:12 +0530

    remove unneeded class definition

--- application/models/Starter.php
+++ application/models/Starter.php
@@ -1,70 +1,5 @@
 <?php
 
-
-class MasterListEntry {
-    public $ID;
-    public $RegisterNumber;
-    public $StudentName;
-    public $RegisteredInstitution;
-    public $BranchName;
-    public $ExamDefinition;
-    public $Slot;
-    public $Course;
-    public $ExamDate;
-    public $ExamTime;
-    public $Session;
-
-    function __construct($ID, $RegisterNumber, $StudentName, $RegisteredInstitution, $BranchName, $ExamDefinition,  $Slot,  $Course,  $ExamDate, $ExamTime, $Session) {
-       $this->ID = $ID;
-       $this->RegisterNumber = $RegisterNumber;
-       $this->StudentName = $StudentName;
-       $this->RegisteredInstitution = $RegisteredInstitution;
-       $this->BranchName = $BranchName;
-       $this->ExamDefinition = $ExamDefinition;
-       $this->Slot = $Slot;
-       $this->Course = $Course;
-       $this->ExamDate = $ExamDate;
-       $this->ExamTime = $ExamTime;
-       $this->Session = $Session;
-
-    }
-    function get_ID() {
-       return $this->ID;
-    }
-    function get_RegisterNumber() {
-       return $this->RegisterNumber;
-    }
-    function get_StudentName() {
-       return $this->StudentName;
-    }
-    function get_RegisteredInstitution() {
-       return $this->RegisteredInstitution;
-    }
-    function get_BranchName() {
-       return $this->BranchName;
-    }
-    function get_ExamDefinition() {
-       return $this->ExamDefinition;
-    }
-    function get_Slot() {
-       return $this->Slot;
-    }
-    function get_Course() {
-       return $this->Course;
-    }
-    function get_ExamDate() {
-       return $this->ExamDate;
-    }
-    function get_ExamTime() {
-       return $this->ExamTime;
-    }
-    function get_Session() {
-       return $this->Session;
-    }
-
- }
-
-
     class Starter extends CI_Model{
 
         function show_data(){
@@ -79,6 +14,5 @@ class MasterListEntry {
             $array_name = $query->result();
 
 //$array_name[0]->ID
-            
         }
     }
